f929f063d0109fefc73abfe2854228c579e25652
hkm1006 <43335420+hkm1006@users.noreply.github.com>
2018-12-29 00:00:59 +0800

    Create ooxx.cpp

---
+++ ooxx.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int player = 0;
+    int winner = 0;
+    int choice = 0;
+    int X = 0;
+    int Y = 0;
+    int line = 0;
+    char JG[3][3] =
+    {
+        {'1','2','3'},
+        {'4','5','6'},
+        {'7','8','9'}
+
+    };
